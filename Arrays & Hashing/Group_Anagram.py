dc04bce6c460d7db1a204973b3eadfdc3476907a
Muhammad Baibars Zain Ul Abideen <baibarsjutt@gmail.com>
2025-10-03 23:09:26 -0500

    Group_Anagram

--- Arrays & Hashing/Group_Anagram.py
+++ Arrays & Hashing/Group_Anagram.py
@@ -1,1 +1,17 @@
-print("Hello")
+class Solution(object):
+    def groupAnagrams(self, strs):
+        from collections import defaultdict
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            
+            key = tuple(count)
+            groups[key].append(word)
+        
+        return list(groups.values())
+ff = Solution()
+print(ff.groupAnagrams(["abc","cba"]))
