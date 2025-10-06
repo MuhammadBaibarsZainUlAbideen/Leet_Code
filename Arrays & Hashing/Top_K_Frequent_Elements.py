d143dfda85a385312cf7bfa69ced2bd8febe107d
Muhammad Baibars Zain Ul Abideen <baibarsjutt@gmail.com>
2025-10-05 23:28:44 -0500

    Top_K_Frequent_Element_o(m log m)

---
+++ Arrays & Hashing/Top_K_Frequent_Elements.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        hash = {}
+        new_hash = {}
+        emp_list = []
+        emplist1 = []
+        for i in nums:
+            hash[i] = hash.get(i,0)+1
+        keys = list(hash.keys())
+        keys.sort()
+        for ii in keys:
+            new_hash[ii] = hash[i]
+        for jf in new_hash:
+            emp_list.append(jf)
+        for i in range(k):
+            emplist1.append(emp_list[i])
+        return emplist1
+
+
+
+
+
+
+cc = Solution()
+print(cc.topKFrequent([1,1,2,3,2],2))
+    
