dc3e6f36fac62179e678ad791ae1c940838727ff
Muhammad Baibars Zain Ul Abideen <baibarsjutt@gmail.com>
2025-10-09 20:38:34 -0500

    Enocde_Decode_String

--- Arrays & Hashing/Enode_Decode_String.py
+++ Arrays & Hashing/Enode_Decode_String.py
@@ -12,8 +12,27 @@ class Solution(object):
 
     def Decode(self):
         calling = self.Encode(["Hello","Poks"])
-        
+        s = calling
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            
+            length = int(s[i:j])
+            
+            word = s[j + 1 : j + 1 + length]
+            res.append(word)
+            
+            i = j + 1 + length
+        return res
+
+               
+
+
+
 
 
 gg = Solution()
-print(gg.Encode(["Hello","Poks"]))
+print(gg.Decode())
