6a1564f3c906d52a7bb9e31c2b5b865ff557a852
Baibars <baibarsjutt@gmail.com>
2025-10-18 19:21:50 -0500

    Reverse_polish_notation

---
+++ Stacks/Reverse_polis_notation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []
+        for token in tokens:
+            if token not in {"+", "-", "*", "/"}:
+                stack.append(int(token))
+            else:
+                b = stack.pop()  
+                a = stack.pop()  
+
+                if token == "+":
+                    stack.append(a + b)
+                elif token == "-":
+                    stack.append(a - b)
+                elif token == "*":
+                    stack.append(a * b)
+                else:
+                    stack.append(int(float(a) / b))  
+        return stack[0]
+ff = Solution()
+print(ff.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
